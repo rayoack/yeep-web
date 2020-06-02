299577958c379a2f9700cd3841cb1a1eb10e1ece
Joel Barbosa <joeljunior250@gmail.com>
2020-06-02 18:41:37 -0300

    payment adjusts in progress

--- src/app/controllers/PaymentController.js
+++ src/app/controllers/PaymentController.js
@@ -1,108 +1,104 @@
 // var paypal = require('paypal-rest-sdk');
-var Paypal = require('paypal-adaptive');
-var request = require('request');
-var paypalSdk = new Paypal({
-        userId:    'sb-50wae1943791_api1.business.example.com',
-        password:  '2D5HW826YH9UYPAX',
-        signature: 'A-9L5sjKRsouMIW.QKy2m.Euta.0AKGLM3JyPyLqgcbiCd1y3IJ9cO.k',
-        sandbox:   true //defaults to false
-});
+// var Paypal = require('paypal-adaptive');
+// var request = require('request');
+// var paypalSdk = new Paypal({
+//         userId:    `${process.env.PAYPAL_USER}`,
+//         password:  `${process.env.PAYPAL_USER_PASSWORD}`,
+//         signature: `${process.env.PAYPAL_USER_SIGNATURE}`,
+//         appId: "Aa6QtPVWZApIKK1im-hiNM-3WZOuHNXGFg8VNN2YvoPmPF003QlSC3hc-YOGkXzeIH14g4Ij0cy_O9DJ",
+//         sandbox:   false //defaults to false
+// });
 
 class PaymentController {
      async index (req, res) {
-        
-        var amount = req.body.amount;
-        var currency = req.body.currency;
+        res.json('success')
 
-        var body = JSON.stringify({
-            actionType: "PAY",
-            senderEmail: "sb-50wae1943791@business.example.com",
-            receiverList: { receiver:
-                [{
-                email: "luke_1346604373_biz@onehappystudent.com",
-                amount: "245"
-                }]},
-            currencyCode: currency,
-            feesPayer: "EACHRECEIVER",
-            memo: "This is a test",
-            cancelUrl: "http://localhost:3333/cancel",
-            returnUrl: "http://localhost:3333/process",
-            ipnNotificationUrl: "http://your_ipn_notification_url",
-            requestEnvelope: {
-                errorLanguage: "en_US" 
-            }
-        })
+        // var amount = req.body.amount;
+        // var currency = req.body.currency;
+        // var receiver_list = req.body.receiver_list;
+        // var body = JSON.stringify({
+        //     actionType: "PAY",
+        //     senderEmail: "sb-2py1u1948028@business.example.com",
+        //     receiverList: { receiver: receiver_list },
+        //     currencyCode: currency,
+        //     feesPayer: "EACHRECEIVER",
+        //     memo: "This is a test",
+        //     cancelUrl: "http://localhost:3333/cancel",
+        //     returnUrl: "http://localhost:3333/process",
+        //     ipnNotificationUrl: "http://your_ipn_notification_url",
+        //     requestEnvelope: {
+        //         errorLanguage: "en_US"
+        //     }
+        // })
 
-        request.post({
-            headers: { 
-                "X-PAYPAL-SECURITY-USERID": "sb-50wae1943791_api1.business.example.com", 
-                "X-PAYPAL-SECURITY-PASSWORD": "2D5HW826YH9UYPAX", 
-                "X-PAYPAL-SECURITY-SIGNATURE": "A-9L5sjKRsouMIW.QKy2m.Euta.0AKGLM3JyPyLqgcbiCd1y3IJ9cO.k.", 
-                "X-PAYPAL-REQUEST-DATA-FORMAT": "JSON", 
-                "X-PAYPAL-RESPONSE-DATA-FORMAT": "JSON", 
-                "X-PAYPAL-APPLICATION-ID": "APP-80W284485P519543T", 
-                "Content-Type": "application/json"
-            },
-            url: 'https://svcs.sandbox.paypal.com/AdaptivePayments/Pay',
-            body: body
-            }, function(error, response, body) {
-                var data = JSON.parse(body)
-                var requestData = {
-                    requestEnvelope: {
-                        errorLanguage:  'en_US',
-                        detailLevel:    'ReturnAll'
-                    },
-                    payKey: data.payKey
-                };
 
-                paypalSdk.callApi('AdaptivePayments/PaymentDetails', requestData, function (err, response) {
-                    if (err) {
-                        // You can see the error
-                        res.json(err)
-                    } else {
-                        // Successful response
-                        var payload = {
-                            requestEnvelope: {
-                                errorLanguage:  'en_US'
-                            },
-                            actionType:     'PAY',
-                            currencyCode:   'USD',
-                            feesPayer:      'EACHRECEIVER',
-                            memo:           'Chained payment example',
-                            cancelUrl:      'http://localhost:3333/cancel',
-                            returnUrl:      'http://localhost:3333/process',
-                            receiverList: {
-                                receiver: [
-                
-                                    {
-                                        email:  'sb-43l9ky1980270@personal.example.com',
-                                        amount: amount*0.2,
-                                        primary:'false'
-                                    },
-                                    {
-                                        email:  'sb-50wae1943791@business.example.com',
-                                        amount: amount*0.8,
-                                        primary:'false'
-                                    }
-                                ]
-                            }
-                        };
-                         
-                        paypalSdk.pay(payload, function (err, response) {
-                            if (err) {
-                                console.log(err);
-                            } else {
-                                // Response will have the original Paypal API response
-                                console.log(response);
-                                // But also a paymentApprovalUrl, so you can redirect the sender to checkout easily
-                                console.log('Redirect to %s', response.paymentApprovalUrl);
-                                res.json({ redirect_url: response.paymentApprovalUrl})
-                            }
-                        });
-                    }
-                });
-        });
-       
+        // request.post({
+        //     headers: {
+        //         "X-PAYPAL-SECURITY-USERID": `${process.env.PAYPAL_USER}`,
+        //         "X-PAYPAL-SECURITY-PASSWORD": `${process.env.PAYPAL_USER_PASSWORD}`,
+        //         "X-PAYPAL-SECURITY-SIGNATURE": `${process.env.PAYPAL_USER_SIGNATURE}`,
+        //         "X-PAYPAL-REQUEST-DATA-FORMAT": "JSON",
+        //         "X-PAYPAL-RESPONSE-DATA-FORMAT": "JSON",
+        //         "X-PAYPAL-APPLICATION-ID": "Aa6QtPVWZApIKK1im-hiNM-3WZOuHNXGFg8VNN2YvoPmPF003QlSC3hc-YOGkXzeIH14g4Ij0cy_O9DJ",
+        //         "Content-Type": "application/json"
+        //     },
+        //     url: 'https://svcs.sandbox.paypal.com/AdaptivePayments/Pay',
+        //     body: body
+        //     }, function(error, response, body) {
+        //         var data = JSON.parse(body)
+        //         var requestData = {
+        //             requestEnvelope: {
+        //                 errorLanguage:  'en_US',
+        //                 detailLevel:    'ReturnAll'
+        //             },
+        //             payKey: data.payKey
+        //         };
+        //         console.log(data)
+
+        //         paypalSdk.callApi('AdaptivePayments/PaymentDetails', requestData, function (err, response) {
+        //             if (err) {
+        //                 // You can see the error
+        //                 res.json(err)
+        //             } else {
+        //                 // Successful response
+        //                 var payload = {
+        //                     requestEnvelope: {
+        //                         errorLanguage:  'en_US'
+        //                     },
+        //                     actionType:     'PAY',
+        //                     currencyCode:   'USD',
+        //                     feesPayer:      'EACHRECEIVER',
+        //                     memo:           'Chained payment example',
+        //                     cancelUrl:      'http://localhost:3333/cancel',
+        //                     returnUrl:      'http://localhost:3333/process',
+        //                     receiverList: {
+        //                         receiver: [
+
+        //                             ...receiver_list,
+        //                             {
+        //                                 email:  process.env.PAYPAL_USER_EMAIL,
+        //                                 amount: amount*0.1,
+        //                                 primary:'false'
+        //                             }
+        //                         ]
+        //                     }
+        //                 };
+
+        //                 paypalSdk.pay(payload, function (err, response) {
+        //                     if (err) {
+        //                         console.log(err);
+        //                     } else {
+        //                         // Response will have the original Paypal API response
+        //                         console.log(response);
+        //                         // But also a paymentApprovalUrl, so you can redirect the sender to checkout easily
+        //                         console.log('Redirect to %s', response.paymentApprovalUrl);
+        //                         res.json({ redirect_url: response.paymentApprovalUrl})
+        //                     }
+        //                 });
+        //             }
+        //         });
+        // });
+
     }
 
     async process(req, res) {
@@ -111,3 +107,4 @@ class PaymentController {
 }
 
 export default new PaymentController()
+
